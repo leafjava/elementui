9ffd5aa99996342d7c40a9dda0822ecabd43189a
DESKTOP-HHM2QH3\ASUS <a894074295@126.com>
2021-06-21 11:27:10 +0800

    初始化

---
+++ src/components/content/TabControl1/TabControl1.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="TabControl1">
+  <div v-for="(item,index) in title"
+       class="tab-control-item1"
+       @click="tabClick(index)"
+        :class="{isActive:index === select}"
+        :style="size">{{item}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl1",
+    props:{
+      title:{
+        type:Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        select:0,
+        transport:0,
+        big:400
+      }
+    },
+    methods:{
+      tabClick(index){
+        this.select = index,
+        this.transport = index
+      }
+    },
+    computed:{
+      panduan() {
+        return this.transport == this.select
+      },
+      size(){
+        return this.panduan ? {fontSize:this.big} : {}
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .TabControl1{
+    display: flex;
+    text-align:center;
+    background-color:#fff;
+
+    position:sticky;
+    top:80px;
+  }
+
+  .tab-control-item1{
+    flex:1;
+    font-size:15px;
+  }
+
+  .isActive {
+    color:red
+  }
+</style>
